d1bb7190a88ef15efc32e29d2c8790ec918149a0
Loris Tissino <loris.tissino@gmail.com>
2019-03-03 20:01:57 +0100

    Lezione prima settimana

---
+++ dam/ex01/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace Exercise01
+{
+    class Program
+    {
+
+        static bool ComprendeLaCifra(int n, int c=7)
+        {
+            while (n>0) {
+                if (n%10==c){
+                    return true;
+                }
+                n = n / 10;
+            }
+            return false;
+        }
+
+        static bool VaSaltato(int n)
+        {
+            if (n%7==0 || ComprendeLaCifra(n)){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+
+        static int ConvertiConControllo(string valore, int def)
+        {
+            try {
+                return Convert.ToInt32(valore);
+            }
+            catch (Exception e) {
+                Console.WriteLine(e.Message);
+                return def;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(args.Length);
+            int min = 0;
+            int max = 100;
+            string mins;
+            string maxs;
+
+            if (args.Length>=2){
+                mins = args[0];
+                maxs = args[1]; 
+            }
+            else
+            {
+                Console.WriteLine("Valore iniziale: ");
+                mins = Console.ReadLine();
+                Console.WriteLine("Valore finale: ");
+                maxs = Console.ReadLine();
+            }
+
+            min = ConvertiConControllo(mins, 0);
+            max = ConvertiConControllo(maxs, 100);
+
+
+            for (int i=min; i<=max; i++)
+            {
+                Console.Write( ( VaSaltato(i) ? "bum": i.ToString()) + " " );
+            }
+
+        }
+    }
+}
---
+++ iot/ex01/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace ex01
+{
+    class Program
+    {
+
+        static bool contieneLaCifra(int n, int c)
+        {
+            while (n!=0){
+                if (n%10==c)
+                {
+                    return true;
+                }
+                n = n / 10;
+            }
+            return false;
+        }
+
+        static bool daSaltare(int n){
+            if (n%7==0 || contieneLaCifra(n, 7))
+            {
+                return true;
+            }
+            else{
+                return false;
+            }            
+        }  
+
+        static int conversioneControllata(string v, int def)
+        {
+            try {
+                return Convert.ToInt32(v);
+            }
+            catch
+            {
+                return def;
+            }            
+        }
+
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(args.Length);
+
+            string minv;
+            string maxv;
+
+            if (args.Length<2)
+            {
+                Console.WriteLine("Valore partenza:");
+                minv = Console.ReadLine();
+                Console.WriteLine("Valore arrivo:");
+                maxv = Console.ReadLine();
+            }
+            else{
+                minv = args[0];
+                maxv = args[1];
+            }
+
+            Console.WriteLine(minv);
+            Console.WriteLine(maxv);
+
+            int min;
+            int max;
+
+            min = conversioneControllata(minv, 0);
+            max = conversioneControllata(maxv, 100);
+
+            int i;
+            for (i=min; i<=max; i++){
+                Console.Write((daSaltare(i)?"bum":i.ToString())+ " ");
+            }
+
+        }
+    }
+}
