74668518d264ada0dcc36237dd2e8c5b7c6a44e2
Loris Tissino <loris.tissino@gmail.com>
2019-03-13 08:47:53 +0100

    Aggiunto esempio programma lettura a rottura di codice

---
+++ dam/csharp/ex02_rotturacodice/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ex02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] a = {  1,  12,  12,  33,  33,  33, 33,  5,  5,  7};
+            int [] b = {100,  10,  30,  40,  70,  80, 90,200, 20, 30};
+
+            Console.WriteLine("a: " + string.Join(", ", a));
+            Console.WriteLine("b: " + string.Join(", ", b));
+
+            int sum = 0;
+            for (int i=0; i<a.Length; i++){
+                sum = sum +  b[i];
+                if (i==a.Length-1 || a[i]!=a[i+1]){
+                    Console.WriteLine("{0,5}: {1,7}", a[i], sum);
+                    sum = 0;
+                }
+            }
+    }
+}
